7192f3d1f9b22b4c9d5a0d8b2cdc02f7f64c1118
Siddhartha Dey <deysiddhartha04@gmail.com>
2025-06-28 11:45:15 +0530

    advanced logging added

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 import express from 'express'
 import 'dotenv/config'
+import logger from "./logger.js";
+import morgan from "morgan";
 
 const app = express()
 const port = process.env.PORT || 3000
+
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 // app.get("/", (req, res) => {
 //     res.send("Hello Sid")
@@ -23,6 +43,8 @@ let nextId = 1;
 
 //add a new tea
 app.post("/teas", (req, res) => {
+    logger.warn("A post is made to add a new tea")
+    
     const {name, price} = req.body;
     const newTea = {
         id: nextId++,
